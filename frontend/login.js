c4f71c47140eb4bbf5a93abd5af1fb284d665767
AlbertoMavungo <mavungo1998@gmail.com>
2023-11-22 17:04:40 -0300

    conectando front and back

--- frontend/login.js
+++ frontend/login.js
@@ -105,7 +105,7 @@ document.querySelector('#btnLoginGoogle').addEventListener('click', function () 
       let user = result.user;
       let idToken = await user.getIdToken();
       
-      fetch('https://projeto-tg-ll-vu9k.vercel.app/', {
+      fetch('https://projeto-tg-ll-vu9k.vercel.app', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
